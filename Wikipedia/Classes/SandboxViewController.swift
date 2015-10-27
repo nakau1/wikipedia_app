fa00ba030cc6f908abd8f4e707b37e9e2c28d141
yuichi <yuichi.nakayasu@gmail.com>
2015-10-28 00:01:09 +0900

    ready sandbox

---
+++ Wikipedia/Classes/SandboxViewController.swift
@@ -0,0 +1,19 @@
+// =============================================================================
+// wikipedia app
+// Copyright (C) 2015年 NeroBlu. All rights reserved.
+// =============================================================================
+import UIKit
+
+/// サンドボックスビューコントローラ
+class SandboxViewController: AppViewController
+{
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+	}
+	
+	@IBAction func didTapTest()
+	{
+		print("TEST")
+	}
+}
