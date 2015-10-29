c2a680512055668216f248a0f95a9ececac50cd7
NakayasuYuichi <y_nakayasu@softolet.com>
2015-10-29 20:01:40 +0900

    一時的なライブラリを作成(一度ここで動作確認をしてNerobluCoreへ回す)

---
+++ TemporaryLibrary/TemporaryLibrary.h
@@ -0,0 +1,19 @@
+//
+//  TemporaryLibrary.h
+//  TemporaryLibrary
+//
+//  Created by user on 2015/10/29.
+//  Copyright © 2015年 NeroBlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TemporaryLibrary.
+FOUNDATION_EXPORT double TemporaryLibraryVersionNumber;
+
+//! Project version string for TemporaryLibrary.
+FOUNDATION_EXPORT const unsigned char TemporaryLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TemporaryLibrary/PublicHeader.h>
+
+
