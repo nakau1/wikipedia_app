6ab2f35525ef256460912ddc0a8aa6a853ce7dd7
NakayasuYuichi <y_nakayasu@softolet.com>
2015-10-30 07:41:00 +0900

    エンティティクラスの追加

---
+++ TemporaryLibrary/NBEntity.swift
@@ -0,0 +1,12 @@
+// =============================================================================
+// NerobluCore
+// Copyright (C) NeroBlu. All rights reserved.
+// =============================================================================
+import UIKit
+import SwiftyJSON
+
+/// エンティティクラス
+public class NBEntity
+{
+	
+}
