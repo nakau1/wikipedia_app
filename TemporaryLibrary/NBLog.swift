e04d917bd14de05058bd4273b5af019b6b7a2c9f
yuichi <yuichi.nakayasu@gmail.com>
2015-11-04 02:05:15 +0900

    ログ出力の実装完了

--- TemporaryLibrary/NBLog.swift
+++ TemporaryLibrary/NBLog.swift
@@ -4,11 +4,22 @@
 // =============================================================================
 import Foundation
 
-// MARK: PUBLIC
+// MARK: - PUBLIC -
+// -----------------------------------------------------------------------------
+// MARK: 関数
+// -----------------------------------------------------------------------------
 
-public func NBLog(target: Any?, title: String? = nil, level: NBLogLevel = .Debug, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
+/// ログ出力を行う
+/// - parameters:
+///   - target: 対象の値
+///   - title: タイトル
+///   - level: ログ出力レベル
+///   - functionName: (常に省略することを推奨)関数(メソッド)名
+///   - fileName: (常に省略することを推奨)ファイル名
+///   - lineNumber: (常に省略することを推奨)行番号
+public func NBLog(target: Any?, _ title: String? = nil, level: NBLogLevel = .Debug, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
 {
-	let option = NBLogOption(
+	let option = NBLoggerOption(
 		title:        title,
 		level:        level,
 		functionName: functionName,
@@ -19,79 +30,252 @@ public func NBLog(target: Any?, title: String? = nil, level: NBLogLevel = .Debug
 	NBLogger(target, option).execute()
 }
 
-public class NBLogConfig
+/// デバッグ用ログ出力を行う
+/// - parameters:
+///   - target: 対象の値
+///   - title: タイトル
+///   - functionName: (常に省略することを推奨)関数(メソッド)名
+///   - fileName: (常に省略することを推奨)ファイル名
+///   - lineNumber: (常に省略することを推奨)行番号
+public func D(target: Any?, _ title: String? = nil, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
 {
-	public static var level = NBLogLevel.Debug // .All
-	
-	public static var filterTitles: [String]? = nil
-	
-	public static var showTitle = true
-	
-	public static var showLogInformation = true
-	
-	public static var showLevel = true
-	
-	public static var showFunctionName = true
-	
-	public static var showFileName = true
-	
-	public static var showLineNumber = true
-	
-	public static var showThread = true
-	
-	public static var showDateTime = true
-	
-	public static var isShortFileName = true
-	
-	public static var dateTimeFormatter: NSDateFormatter {
-		get {
-			if let dtf = _dateTimeFormatter {
-				return dtf
-			}
-			
-			let dtf = NSDateFormatter()
-			dtf.locale     = NSLocale.currentLocale()
-			dtf.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-			
-			_dateTimeFormatter = dtf
-			return dtf
-		}
-	}
-	private static var _dateTimeFormatter: NSDateFormatter?
+	NBLog(target, title, level: .Debug, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
 }
 
-public enum NBLogLevel
+/// エラーログ出力を行う
+/// - parameters:
+///   - target: 対象の値
+///   - title: タイトル
+///   - functionName: (常に省略することを推奨)関数(メソッド)名
+///   - fileName: (常に省略することを推奨)ファイル名
+///   - lineNumber: (常に省略することを推奨)行番号
+public func E(target: Any?, _ title: String? = nil, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
 {
-	case Debug
-	case Information
-	case Warning
-	case Error
-	case Fetal
-	case Query
-	case Request
-	case Response
-	case Event
+	NBLog(target, title, level: .Error, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+}
+
+/// 情報ログ出力を行う
+/// - parameters:
+///   - target: 対象の値
+///   - title: タイトル
+///   - functionName: (常に省略することを推奨)関数(メソッド)名
+///   - fileName: (常に省略することを推奨)ファイル名
+///   - lineNumber: (常に省略することを推奨)行番号
+public func I(target: Any?, _ title: String? = nil, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
+{
+	NBLog(target, title, level: .Information, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+}
+
+/// 警告ログ出力を行う
+/// - parameters:
+///   - target: 対象の値
+///   - title: タイトル
+///   - functionName: (常に省略することを推奨)関数(メソッド)名
+///   - fileName: (常に省略することを推奨)ファイル名
+///   - lineNumber: (常に省略することを推奨)行番号
+public func W(target: Any?, _ title: String? = nil, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
+{
+	NBLog(target, title, level: .Warning, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+}
+
+/// イベントログ出力を行う
+/// - parameters:
+///   - target: 対象の値
+///   - title: タイトル
+///   - functionName: (常に省略することを推奨)関数(メソッド)名
+///   - fileName: (常に省略することを推奨)ファイル名
+///   - lineNumber: (常に省略することを推奨)行番号
+public func ev(target: Any?, _ title: String? = nil, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__)
+{
+	NBLog(target, title, level: .Event, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+}
+
+// -----------------------------------------------------------------------------
+// MARK: 出力レベル
+// -----------------------------------------------------------------------------
+
+/// 出力レベルをビット定義する構造体
+public struct NBLogLevel : OptionSetType
+{
+	public let  rawValue: Int
+	public init(rawValue: Int) { self.rawValue = rawValue }
+	
+	public static let Debug       = NBLogLevel(rawValue: 1 << 0)
+	public static let Information = NBLogLevel(rawValue: 1 << 1)
+	public static let Warning     = NBLogLevel(rawValue: 1 << 2)
+	public static let Error       = NBLogLevel(rawValue: 1 << 3)
+	public static let Fetal       = NBLogLevel(rawValue: 1 << 4)
+	public static let Query       = NBLogLevel(rawValue: 1 << 5)
+	public static let QueryResult = NBLogLevel(rawValue: 1 << 6)
+	public static let Request     = NBLogLevel(rawValue: 1 << 7)
+	public static let Response    = NBLogLevel(rawValue: 1 << 8)
+	public static let Event       = NBLogLevel(rawValue: 1 << 9)
+	
+	public static let None     = NBLogLevel([])
+	public static let Problems = NBLogLevel([.Warning, .Error, .Fetal])
+	public static let DB       = NBLogLevel([.Query, .QueryResult])
+	public static let API      = NBLogLevel([.Request, .Response])
+	public static let Verbose  = NBLogLevel([.Debug, .Information, .Event])
+	public static let All      = NBLogLevel([.Problems, .DB, .API, .Verbose])
 	
 	internal var shortName: String {
 		get {
-			switch self {
-				case .Debug:       return "D"
-				case .Information: return "I"
-				case .Warning:     return "W"
-				case .Error:       return "E"
-				case .Fetal:       return "F"
-				case .Query:       return "Q"
-				case .Request:     return "q"
-				case .Response:    return "r"
-				case .Event:       return "e"
+			switch self.rawValue {
+			case NBLogLevel.Debug       .rawValue: return "D"
+			case NBLogLevel.Information .rawValue: return "I"
+			case NBLogLevel.Warning     .rawValue: return "W"
+			case NBLogLevel.Error       .rawValue: return "E"
+			case NBLogLevel.Fetal       .rawValue: return "F"
+			case NBLogLevel.Query       .rawValue: return "Q"
+			case NBLogLevel.QueryResult .rawValue: return "R"
+			case NBLogLevel.Request     .rawValue: return "q"
+			case NBLogLevel.Response    .rawValue: return "r"
+			case NBLogLevel.Event       .rawValue: return "e"
+			default: return " "
 			}
 		}
 	}
 }
 
-// MARK: PRIVATE (Not-Accessable)
+// -----------------------------------------------------------------------------
+// MARK: 出力設定
+// -----------------------------------------------------------------------------
 
-internal struct NBLogOption
+/// ログ出力の日付フォーマット
+public enum NBLogDateFormat : String
+{
+	case Default    = "HH:mm:ss.SSS"
+	case Time       = "HH:mm:ss"
+	case DateTime   = "yyyy-MM-dd HH:mm:ss"
+	case Punctual   = "yyyy-MM-dd HH:mm:ss.SSS"
+	case Continuous = "mm.ss.SSS"
+}
+
+// -----------------------------------------------------------------------------
+// MARK: 出力設定
+// -----------------------------------------------------------------------------
+
+/// ログ出力の設定を行うための静的なクラス
+public class NBLogConfig
+{
+	/// 出力そのものの有効/無効
+	public static var enabled = true
+	
+	/// フィルタする出力レベル
+	/// 
+	/// 下例のように設定するとDebugのみが出力される
+	///
+	///	NBLogConfig.filterLevel = NBLogLevel([.Debug])
+	///
+	/// 下例のように設定するとすべて出力される(初期値)
+	///
+	///	NBLogConfig.filterLevel = nil
+	///
+	public static var filterLevel: NBLogLevel? = nil
+	
+	/// フィルタするタイトル
+	///
+	/// 下例のように設定すると"ok"とタイトル設定したログのみが出力される
+	///
+	///	NBLogConfig.filterTitles = ["ok"]
+	///	D("出力される", 　"ok")
+	///	D("出力されない", "ng")
+	///
+	public static var filterTitles: [String]? = nil
+	
+	/// 出力時にタイトルを出力するかどうか
+	public static var showTitle = true
+	
+	/// 出力時にログ詳細情報を出力するかどうか
+	/// 
+	/// ログ詳細情報とはタイトルと出力対象以外の情報を指します
+	public static var showLogInformation = true
+	
+	/// 出力時にログ出力レベル(アルファベット1文字)を出力するかどうか
+	public static var showLevel = false
+	
+	/// 出力時に関数名(メソッド名)を出力するかどうか
+	public static var showFunctionName = false
+	
+	/// 出力時にファイル名を出力するかどうか
+	public static var showFileName = true
+	
+	/// 出力時に行番号を出力するかどうか
+	public static var showLineNumber = true
+	
+	/// 出力時にスレッド名を出力するかどうか
+	public static var showThread = false
+	
+	/// 出力時に出力日時を出力するかどうか
+	public static var showDateTime = true
+	
+	/// ファイル名を出力する場合、ファイル名だけを出力するかどうか
+	/// 
+	/// true:ファイル名のみ、false:フルパス
+	public static var isShortFileName = true
+	
+	/// 出力日時を出力する際の日付フォーマット
+	public static var dateFormat: NBLogDateFormat = .Default
+	
+	/// すべて出力する設定に変更する
+	public class func configureShowAll()
+	{
+		NBLogConfig.filterLevel        = nil
+		NBLogConfig.filterTitles       = nil
+		NBLogConfig.showTitle          = true
+		NBLogConfig.showLogInformation = true
+		NBLogConfig.showLevel          = true
+		NBLogConfig.showFunctionName   = true
+		NBLogConfig.showFileName       = true
+		NBLogConfig.showLineNumber     = true
+		NBLogConfig.showThread         = true
+		NBLogConfig.showDateTime       = true
+		NBLogConfig.isShortFileName    = false
+		NBLogConfig.dateFormat         = .Punctual
+	}
+	
+	/// シンプルな出力をする設定に変更する
+	public class func configureSimply()
+	{
+		NBLogConfig.filterLevel        = nil
+		NBLogConfig.filterTitles       = nil
+		NBLogConfig.showTitle          = true
+		NBLogConfig.showLogInformation = true
+		NBLogConfig.showLevel          = false
+		NBLogConfig.showFunctionName   = false
+		NBLogConfig.showFileName       = true
+		NBLogConfig.showLineNumber     = true
+		NBLogConfig.showThread         = false
+		NBLogConfig.showDateTime       = true
+		NBLogConfig.isShortFileName    = true
+		NBLogConfig.dateFormat         = .Time
+	}
+	
+	/// 連続したイベント出力に適した設定に変更する
+	public class func configureContinuous()
+	{
+		NBLogConfig.filterLevel        = NBLogLevel([.Event])
+		NBLogConfig.filterTitles       = nil
+		NBLogConfig.showTitle          = true
+		NBLogConfig.showLogInformation = true
+		NBLogConfig.showLevel          = false
+		NBLogConfig.showFunctionName   = false
+		NBLogConfig.showFileName       = false
+		NBLogConfig.showLineNumber     = false
+		NBLogConfig.showThread         = false
+		NBLogConfig.showDateTime       = true
+		NBLogConfig.isShortFileName    = true
+		NBLogConfig.dateFormat         = .Continuous
+	}
+}
+
+// MARK: - PRIVATE (Not-Accessable) -
+// -----------------------------------------------------------------------------
+// MARK: 出力設定
+// -----------------------------------------------------------------------------
+
+/// NBLogger用のオプション用構造体
+internal struct NBLoggerOption
 {
 	var title:        String?
 	var level:        NBLogLevel
@@ -100,12 +284,13 @@ internal struct NBLogOption
 	var lineNumber:   Int
 }
 
+/// ログ出力を実際に行うクラス
 internal class NBLogger
 {
 	let target: Any?
-	let option: NBLogOption
+	let option: NBLoggerOption
 	
-	init(_ target: Any?, _ option: NBLogOption)
+	init(_ target: Any?, _ option: NBLoggerOption)
 	{
 		self.target = target
 		self.option = option
@@ -113,22 +298,49 @@ internal class NBLogger
 	
 	internal func execute()
 	{
-		var logInformation = ""
-		if NBLogConfig.showLogInformation {
-			logInformation = self.logInformationsText()
-			if !logInformation.isEmpty {
-				logInformation = "[\(logInformation)] "
-			}
+		if !NBLogConfig.enabled || !self.isFilteredTitle() || !self.isFilteredLevel() {
+			return
 		}
 		
-		print(logInformation)
+		var logInformation = ""
+		if let v = self.logInformationsText() {
+			logInformation = "[\(v)] "
+		}
+		
+		var title = ""
+		if let t = self.titleText() {
+			title = "'\(t)' > "
+		}
+		
+		print("\(logInformation)\(title)\(self.fixedTarget())")
 	}
 	
-	private func logInformationsText()->String
+	private func isFilteredTitle()->Bool
 	{
+		guard
+			let filterTitles = NBLogConfig.filterTitles,
+			let title = self.option.title
+			else {
+			return true
+		}
+		return filterTitles.contains(title)
+	}
+	
+	private func isFilteredLevel()->Bool
+	{
+		guard let filterLevel = NBLogConfig.filterLevel else {
+			return true
+		}
+		return filterLevel.contains(self.option.level)
+	}
+	
+	private func logInformationsText()->String?
+	{
+		if !NBLogConfig.showLogInformation { return nil }
+		
 		let logInformationTextClosures: [()->String?] = [
-			{ return self.dateTimeLogInformationText() },
 			{ return self.logLevelLogInformationText() },
+			{ return self.dateTimeLogInformationText() },
 			{ return self.threadNameLogInformationText() },
 			{ return self.fileNameLogInformationText() },
 			{ return self.lineNumberLogInformationText() },
@@ -141,11 +353,25 @@ internal class NBLogger
 				texts.append(logInformationText)
 			}
 		}
+		if texts.isEmpty { return nil }
 		
 		return (texts as NSArray).componentsJoinedByString(" ")
 	}
 	
-	private func titleLogInformationText()->String?
+	private func fixedTarget()->Any
+	{
+		if let value = self.target {
+			if let text = value as? String {
+				return "\"\(text)\""
+			} else {
+				return value
+			}
+		} else {
+			return "(nil)"
+		}
+	}
+	
+	private func titleText()->String?
 	{
 		if !NBLogConfig.showTitle { return nil }
 		return self.option.title
@@ -154,7 +380,12 @@ internal class NBLogger
 	private func dateTimeLogInformationText()->String?
 	{
 		if !NBLogConfig.showDateTime { return nil }
-		return NBLogConfig.dateTimeFormatter.stringFromDate(NSDate())
+		
+		let dtf = NSDateFormatter()
+		dtf.locale     = NSLocale.currentLocale()
+		dtf.dateFormat = NBLogConfig.dateFormat.rawValue
+		
+		return dtf.stringFromDate(NSDate())
 	}
 	
 	private func logLevelLogInformationText()->String?
@@ -177,7 +408,7 @@ internal class NBLogger
 	private func lineNumberLogInformationText()->String?
 	{
 		if !NBLogConfig.showLineNumber { return nil }
-		return "line:\(self.option.lineNumber)"
+		return "(\(self.option.lineNumber))"
 	}
 	
 	private func functionNameLogInformationText()->String?
--- Wikipedia/Classes/Schenes/Main/MainViewController.swift
+++ Wikipedia/Classes/Schenes/Main/MainViewController.swift
@@ -14,28 +14,30 @@ class MainViewController: AppViewController
 	{
 		super.viewDidLoad()
 		
-		NBLogConfig.showLogInformation = false
+		NBLogConfig.enabled = (DEBUG == 1)
+		NBLogConfig.configureSimply()
 		
 		NBLog(self)
 		NBLog(1)
 		NBLog(true)
 		NBLog("1")
 		NBLog("")
-		NBLog(NSData())
+		NBLog(NSURL(string: "http://www.www.ww"))
 		NBLog(1.0 * 3.56)
 		NBLog(Character("a"))
 		NBLog(["1", "2"])
 		NBLog([String]())
 		NBLog([Int!]())
-		NBLog(["1" : "2"])
+		NBLog(["1" : ""])
 		NBLog([String:String]())
 		NBLog(MainViewController.self)
 		NBLog((o: "o", 21, pot: NSDate()))
     
 		let h : H = {_ in }
 		NBLog(h)
-		
 		NBLog(NBLogLevel.Error)
 		NBLog(nil)
+		
+		
 	}
 }
