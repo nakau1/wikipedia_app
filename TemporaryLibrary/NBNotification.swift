eb245c30bc3e48254ecd588fc9dbe67866066021
yuichi <yuichi.nakayasu@gmail.com>
2015-11-04 15:46:32 +0900

    ビューコントローラの基底クラスと、通知に関する処理を追加

---
+++ TemporaryLibrary/NBNotification.swift
@@ -0,0 +1,57 @@
+// =============================================================================
+// NerobluCore
+// Copyright (C) NeroBlu. All rights reserved.
+// =============================================================================
+import UIKit
+
+/// 通知の処理を行うクラス
+public extension NSNotificationCenter
+{
+	/// 通知の監視開始/終了の設定を一括で行う
+	/// - parameters:
+	///   - selectorAndNotificationNames: 通知時に実行するセレクタ名と通知文字列をセットにした辞書
+	///   - observer: 監視するオブジェクト
+	///   - start: 監視の開始または終了
+	public func observeNotifications(selectorAndNotificationNames: [String : String], observer: AnyObject, start: Bool)
+	{
+		for e in selectorAndNotificationNames {
+			let name = e.1
+			if start {
+				let selector = Selector(e.0)
+				self.addObserver(observer, selector: selector, name: name, object: nil)
+			} else {
+				self.removeObserver(observer, name: name, object: nil)
+			}
+		}
+	}
+}
+
+public extension NBViewController
+{
+	/// 監視する通知とそのセレクタの設定を返却する
+	/// 
+	/// 例えば下記のように実装するとキーボードイベントがハンドルできます
+	///
+	///	func keyboardWillChangeFrame(n: NSNotification) {
+	///	  // 何か処理
+	///	}
+	///
+	///	override func observingNotifications() -> [String : String] {
+	///	  var ret = super.observingNotifications()
+	///	  ret["keyboardWillChangeFrame:"] = UIKeyboardWillChangeFrameNotification
+	///	  return ret
+	///	}
+	///
+	/// - returns: 通知時に実行するセレクタ名と通知文字列をセットにした辞書
+	public func observingNotifications()->[String : String] { return [:] }
+	
+	public override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		NSNotificationCenter.defaultCenter().observeNotifications(self.observingNotifications(), observer:self, start: true)
+	}
+	
+	public override func viewDidDisappear(animated: Bool) {
+		super.viewDidDisappear(animated)
+		NSNotificationCenter.defaultCenter().observeNotifications(self.observingNotifications(), observer:self, start: false)
+	}
+}
--- Wikipedia/Classes/Bases/AppViewController.swift
+++ Wikipedia/Classes/Bases/AppViewController.swift
@@ -3,9 +3,10 @@
 // Copyright (C) 2015年 NeroBlu. All rights reserved.
 // =============================================================================
 import UIKit
+import TemporaryLibrary
 
 /// アプリ内ビューコントローラの基底クラス
-class AppViewController: UIViewController
+class AppViewController: NBViewController
 {
 	override func viewDidLoad()
 	{
