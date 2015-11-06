653bb9b969188926b9e3a7b1156097f0c08b564f
NakayasuYuichi <y_nakayasu@softolet.com>
2015-11-06 17:08:08 +0900

    メイン画面のレイアウト作成

--- Wikipedia/Classes/Schenes/Main/MainViewController.swift
+++ Wikipedia/Classes/Schenes/Main/MainViewController.swift
@@ -6,10 +6,13 @@ import UIKit
 import TemporaryLibrary
 
 /// メイン画面ビューコントローラ
-class MainViewController: AppViewController
+class MainViewController: AppViewController, UITextFieldDelegate
 {
+  @IBOutlet private weak var titleView: UIView!
+  
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
+		self.navigationItem.titleView = self.titleView
 	}
 }
