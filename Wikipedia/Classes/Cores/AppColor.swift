e0e815f42774e2a6e94825bc6906bd8e121c1227
yuichi <yuichi.nakayasu@gmail.com>
2015-11-10 17:28:08 +0900

    アプリの色定義を追加した

--- Wikipedia/Classes/Cores/AppColor.swift
+++ Wikipedia/Classes/Cores/AppColor.swift
@@ -7,5 +7,13 @@ import UIKit
 /// アプリ内で使用する色の定義
 class AppColor
 {
-	
+	/// 基調色
+	static let Keynote = UIColor.colorWithRGB(0x8BC24A)
+	static let KeynoteDark = UIColor.colorWithRGB(0x416021)
+	/// 基調背景色
+	static let Background = UIColor.colorWithRGB(0x242424)
+	/// 基本文字色
+	static let Text = UIColor.colorWithRGB(0x565C59)
+	/// プレースホルダ
+	static let Placeholder = UIColor.colorWithRGB(0xB4B496)
 }
