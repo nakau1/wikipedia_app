fb98f25d9bfb4cd34e73f6906e396fd8c6a59f96
yuichi <yuichi.nakayasu@gmail.com>
2015-11-10 17:25:48 +0900

    汎用定義用ファイル追加

---
+++ TemporaryLibrary/NB.swift
@@ -0,0 +1,18 @@
+// =============================================================================
+// NerobluCore
+// Copyright (C) NeroBlu. All rights reserved.
+// =============================================================================
+import UIKit
+
+/// 汎用定義
+public struct NB
+{
+	/// 画面
+	public struct SCREEN
+	{
+		/// 画面横幅
+		public static let WIDTH = UIScreen.mainScreen().bounds.size.width
+		/// 画面縦幅
+		public static let HEIGHT = UIScreen.mainScreen().bounds.size.height
+	}
+}
