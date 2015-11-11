430f8612ae724562d54593c875e602b0c618806a
yuichi <yuichi.nakayasu@gmail.com>
2015-11-11 14:29:49 +0900

    検索画面を追加

---
+++ Wikipedia/Classes/Schenes/Search/SearchViewController.swift
@@ -0,0 +1,15 @@
+// =============================================================================
+// wikipedia app
+// Copyright (C) 2015年 NeroBlu. All rights reserved.
+// =============================================================================
+import UIKit
+import TemporaryLibrary
+
+/// 検索画面ビューコントローラ
+class SearchViewController: AppViewController
+{
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+	}
+}
