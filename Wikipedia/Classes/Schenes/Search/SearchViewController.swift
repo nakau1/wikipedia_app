e442d3d35b979022f506e54449d0be77df97ba92
yuichi <yuichi.nakayasu@gmail.com>
2015-11-11 16:04:32 +0900

    検索画面のレイアウト更新と、画面遷移実装

--- Wikipedia/Classes/Schenes/Search/SearchViewController.swift
+++ Wikipedia/Classes/Schenes/Search/SearchViewController.swift
@@ -8,8 +8,21 @@ import TemporaryLibrary
 /// 検索画面ビューコントローラ
 class SearchViewController: AppViewController
 {
+	@IBOutlet private weak var txfSearch: UITextField!
+	
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
+		self.modalTransitionStyle = .CrossDissolve
+		
+		let placeholderText = AppString.Search.PlaceholderText
+		txfSearch.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSForegroundColorAttributeName : AppColor.Placeholder])
+	}
+	
+	@IBAction private func didTapClose()
+	{
+		self.dismissViewControllerAnimated(true) {
+			
+		}
 	}
 }
